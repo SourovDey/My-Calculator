bae2936358894d7379934a343067d7280a4e46a7
SourovDey <developer.sourovdey@gmail.com>
2023-01-05 00:54:00 +0600

    calculator last update class-04

---
+++ main.js
@@ -0,0 +1,301 @@
+// console.log("total ", 5 + 5, 7 * 7);
+//       alert("hello World");
+//       var , let , const
+//       var name = "pappu dey";
+//       name = "Swarna ";
+//       var roll = 18;
+//       var name = "swarna ", roll = 18;
+//       var name = "Sourov";
+//       let name = "Nasifa";
+//       name = "Pappu Dey";
+//       const name = "Anas";
+//        name = "Saad";
+//       console.log(name);
+      
+
+
+// let number1 = 30;
+// let number2 = 20;
+
+
+// let $ = 10;
+// let _ = 20
+
+// let firstName = "sourov";
+// let lastName = "Dey";
+
+// console.log(firstName+ lastName);
+// document.write(firstName + " " + lastName)
+
+
+/*
+console.log("total ", 5 + 5, 7 * 7);
+      alert("hello World");
+      var , let , const
+      var name = "pappu dey";
+      name = "Swarna ";
+      var roll = 18;
+      var name = "swarna ", roll = 18;
+      var name = "Sourov";
+      let name = "Nasifa";
+      name = "Pappu Dey";
+      const name = "Anas";
+       name = "Saad";
+      console.log(name);
+      */
+
+
+// let number1 = 30;
+// let number2 = 20;
+
+
+// let $ = 10;
+// let _ = 20
+
+// let firstName = "pappu";
+// let lastName = "Dey";
+
+// console.log(firstName+ lastName);
+// document.write(firstName + " " + lastName)
+
+
+// console.log( 10 );
+// console.log(34.22);
+
+// console.log(10+ 20)
+
+// console.log(30 + 5.06)
+
+
+// object class 
+
+// const personDetails = {
+//       name : "sourov",
+//       roll : 25,
+// }
+
+// let name = "Coder Pappu";
+
+/*
+const studentDetails = {
+      student1 : {
+            name : "sourov",
+            roll : 12,
+            department : "Computer Technology",
+            semester : "4th",
+            group : "B1"
+      },
+      student2 : {
+            name : {
+                  firstName : "Khalifa",
+                  lastName : "Anas"
+            },
+            roll : 10,
+            department : "computer technology ",
+            semester : "4th",
+            group  : "B1"
+      }
+}
+let studentFirstName = studentDetails.student2.name.firstName;
+let studentLastName = studentDetails.student2.name.lastName;
+let fullName = studentFirstName + " " +  studentLastName;
+console.log(fullName)
+*/
+
+
+// console.log(studentDetails.student1.department)
+// console.warn(studentDetails)
+// console.error(studentDetails)
+
+// Array 
+// let personDetails = [
+//       {
+//       preson1 : "perosn name " 
+//       },
+//       "person2", "person3"]
+// console.log(personDetails)
+
+
+// let name
+
+// console.log(name)
+
+
+// console.log(studentDetails.student1.department)
+// console.warn(studentDetails)
+// console.error(studentDetails)
+
+// Array 
+// let personDetails = [
+//       {
+//       preson1 : "perosn name " 
+//       },
+//       "person2", "person3"]
+// console.log(personDetails)
+
+
+// let name
+
+// console.log(name)
+
+// let userName = prompt("Enter Your name ", "Pappu Dey")
+// console.log(userName)
+
+
+// let confirmValue = confirm("Enter your name ")
+
+// console.log(confirmValue)
+
+// let sumNum = 10;
+// console.log(sumNum++)
+
+
+// let decNum  = 19;
+// console.log(decNum--)
+
+// console.log(19**2)
+
+// let x = 10;
+// let y = 20;
+// console.log(x+=y)
+
+let numOne = 10;
+let numTwo = 20;
+let sumOrDec = prompt("Enter a operator ", "")
+
+
+
+// console.log(numOne++)
+
+if ( sumOrDec !== "-"){
+
+      let numOneCheck = numOne > 0 ? numOne : "False";
+      let numTwoCheck = numTwo > 0 ? numTwo : "False"
+
+
+      if (numOneCheck === "False" ){
+            console.log("Number Error");
+      }
+      else if(numTwoCheck === "False"){
+            console.log("Number Error");
+      }
+      else{
+
+            let sum = numOneCheck + numTwoCheck;
+            console.log(sum)
+
+      }
+      
+      
+}
+else if (sumOrDec !== "+"){
+      
+
+      let numOneCheck = numOne > 0 ? numOne : "False";
+      let numTwoCheck = numTwo > 0 ? numTwo : "False"
+
+
+      if (numOneCheck === "False" ){
+            console.log("Number Error");
+      }
+      else if(numTwoCheck === "False"){
+            console.log("Number Error");
+      }
+      else{
+
+            let sum = numOneCheck - numTwoCheck;
+            console.log(sum)
+
+      }
+}
+else {
+      console.log("Error")
+}
+
+
+
+
+
+    //    -------operator work using if else----- 
+let numberOne = 50;
+
+let numberTwo = 30; 
+        let operator = prompt("Enter a operator :", ""); 
+        let FirstNoCheck=0; 
+        let SecNoCheck=0; 
+        let sum =0; 
+ 
+        if (operator === "+"){ 
+               FirstNoCheck = numberOne > 0 ? numberOne : 0; 
+               SecNoCheck = numberTwo > 0 ? numberTwo : 0; 
+            if (FirstNoCheck === "F"){ 
+               console.log("Invalid Number"); 
+              } 
+               else if(SecNoCheck === "F"){ 
+                console.log("Invalid Number"); 
+             } else{ 
+                  sum = FirstNoCheck + SecNoCheck; 
+                console.log(sum); 
+              } 
+ 
+        } 
+        else if (operator === "-") { 
+                FirstNoCheck = numberOne > 0 ? numberOne : 0; 
+                SecNoCheck = numberTwo > 0 ? numberTwo : 0; 
+            if (FirstNoCheck === "F"){ 
+               console.log("Invalid Number"); 
+              } 
+               else if(SecNoCheck === "F"){ 
+                console.log("Invalid Number"); 
+             } else{ 
+                   sum = FirstNoCheck - SecNoCheck; 
+                console.log(sum); 
+              } 
+ 
+        } 
+        else if (operator === "*") { 
+               FirstNoCheck = numberOne > 0 ? numberOne : 0; 
+               SecNoCheck = numberTwo > 0 ? numberTwo : 0; 
+            if (FirstNoCheck === "F"){ 
+               console.log("Invalid Number"); 
+              } 
+               else if(SecNoCheck === "F"){ 
+                console.log("Invalid Number"); 
+             } else{ 
+               sum = FirstNoCheck * SecNoCheck; 
+                console.log(sum); 
+              } 
+ 
+        } 
+        else if (operator === "/") { 
+               FirstNoCheck = numberOne > 0 ? numberOne : 0; 
+               SecNoCheck = numberTwo > 0 ? numberTwo : 0; 
+            if (FirstNoCheck === "F"){ 
+               console.log("Invalid Number"); 
+              } 
+               else if(SecNoCheck === "F"){ 
+                console.log("Invalid Number"); 
+             } else{ 
+                  sum = FirstNoCheck / SecNoCheck; 
+                console.log(sum); 
+              } 
+ 
+        } 
+        else{ (operator === "%")  
+               FirstNoCheck = numberOne > 0 ? numberOne : 0; 
+               SecNoCheck = numberTwo > 0 ? numberTwo : 0; 
+            if (FirstNoCheck === "F"){ 
+               console.log("Invalid Number"); 
+              } 
+               else if(SecNoCheck === "F"){ 
+                console.log("Invalid Number"); 
+             } else{ 
+                 sum = FirstNoCheck % SecNoCheck; 
+                console.log(sum); 
+              } 
+ 
+        } 
+        document.getElementById("calOne").innerHTML=FirstNoCheck; 
+        document.getElementById("calTre").innerHTML=SecNoCheck; 
+        document.getElementById("calTwo").innerHTML=operator; 
+        document.getElementById("answer").innerHTML=sum; 
